749791e660881266e13c07da6f8ae7f21a3b5462
sergey.alekseev <demiurg58@gmail.com>
2024-07-15 22:04:48 +0300

    edit url

--- sa-frontend/src/App.js
+++ sa-frontend/src/App.js
@@ -20,7 +20,7 @@ class App extends Component {
     };
 
     analyzeSentence() {
-        fetch('http://0.0.0.0:8080/sentiment', {
+        fetch('http://192.168.0.108:8080/sentiment', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
