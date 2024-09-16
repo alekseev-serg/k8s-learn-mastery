9703c1bb21081260f23db6e9f07a0378ca43301a
sergey.alekseev <demiurg58@gmail.com>
2024-09-16 09:05:53 +0300

    done minikube learn and up the App

--- sa-frontend/src/App.js
+++ sa-frontend/src/App.js
@@ -20,7 +20,7 @@ class App extends Component {
     };
 
     analyzeSentence() {
-        fetch('http://192.168.0.108:8080/sentiment', {
+        fetch('http://192.168.49.2:32722/sentiment', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
